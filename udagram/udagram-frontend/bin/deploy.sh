9f093609fe0ae6f02e7691217aa576c1dd7f7ff2
buicongtien-cloud <39719513+buicongtien@users.noreply.github.com>
2024-08-24 21:51:59 +0700

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,9 +1,2 @@
-# Define bucket name and access point
-S3_BUCKET_NAME="udagram-frontend-tienbc"
-ACCESS_POINT="arn:aws:s3:us-west-2:693133310848:accesspoint/udagram-frontend-tienbc"
-
-# Upload all files in the ./dist directory
-aws s3 cp --recursive --acl public-read ./dist s3://$S3_BUCKET_NAME/
-
-# Upload index.html with specific cache control settings
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/index.html s3://$S3_BUCKET_NAME/
+aws s3 cp --recursive --acl public-read ./dist s3://udagram-frontend-tienbc/ --region us-west-2
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/index.html s3://udagram-frontend-tienbc/ --region us-west-2
