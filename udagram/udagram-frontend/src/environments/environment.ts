dc59b8eb61c8386629052c943b0f6557ae85e478
buicongtien-cloud <39719513+buicongtien@users.noreply.github.com>
2024-08-24 23:39:18 +0700

    Update environment.ts

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-host-env.eba-5ramtjbq.us-west-2.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-env.eba-jtry7a6m.us-west-2.elasticbeanstalk.com//api/v0'
 };
 
 /*
