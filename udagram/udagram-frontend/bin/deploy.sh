5b4f38bbff13f2c0e6c4d2aa1a3cba4ead74d219
buicongtien-cloud <39719513+buicongtien@users.noreply.github.com>
2024-08-24 21:16:52 +0700

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./dist s3://arn:udagram-frontend-tienbc/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/index.html s3://arn:udagram-frontend-tienbc/
+aws s3 cp --recursive --acl public-read ./dist s3://arn:aws:s3:::udagram-frontend-tienbc/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/index.html s3://arn:aws:s3:::udagram-frontend-tienbc/
